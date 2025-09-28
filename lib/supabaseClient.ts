3b9f2799fb8d343d53516ad5559a6068253a1fbc
Yuri <106544693+LilyCalico@users.noreply.github.com>
2025-09-29 04:10:35 +0900

    feat: Add Supabase type generation script and update supabaseClient with type import

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,9 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
+import type { Database } from "../types/supabasetype";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
-
-export type Database = {};
 
 if (!supabaseUrl || !supabaseAnonKey) {
   // eslint-disable-next-line no-console
