f99fbe5b4adca7d06e8e0579a73322d3be45310f
Yuri <106544693+LilyCalico@users.noreply.github.com>
2025-10-24 04:03:56 +0900

    feat: Add TripSchedulePage component to display trip date based on URL query

---
+++ pages/trips/[tripId]/schedule.tsx
@@ -0,0 +1,19 @@
+import { useRouter } from "next/router";
+import { useMemo } from "react";
+import { useCurrentTrip } from "@/hooks/useCurrentTrip";
+
+export default function TripSchedulePage() {
+  const { query } = useRouter();
+  // 必要があればcurrentTripを更新
+  useCurrentTrip();
+
+  const dateStr = useMemo(() => {
+    return typeof query.date === "string" ? query.date : "";
+  }, [query.date]);
+
+  return (
+    <div>
+      <h1 className="text-[1.6rem] font-bold text-center">{dateStr}</h1>
+    </div>
+  );
+}
