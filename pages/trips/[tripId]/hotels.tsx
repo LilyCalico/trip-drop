98feae80dfd543cfa9e494ccdc6bebaf47de246f
Yuri <106544693+LilyCalico@users.noreply.github.com>
2025-11-05 01:38:49 +0900

    feat: Add transports and hotels page

---
+++ pages/trips/[tripId]/hotels.tsx
@@ -0,0 +1,10 @@
+import HeaderDate from "@/components/custom/layout/HeaderDate";
+
+export default function HotelsPage() {
+  return (
+    <>
+      <HeaderDate category="hotel" />
+      <div></div>
+    </>
+  );
+}
