4132c44519f49953b9107bcbd519721da05e23b3
Yuri <106544693+LilyCalico@users.noreply.github.com>
2025-11-12 04:38:28 +0900

    style: Update font size

--- components/custom/Input.tsx
+++ components/custom/Input.tsx
@@ -8,9 +8,9 @@ export function Input({ className, ...props }: Props) {
     <BaseInput
       className={cn(
         // 背景・余白・フォントサイズを移植
-        "py-[1.6rem] text-[1.4rem] mt-[0.4rem]",
+        "py-[1.6rem] text-[1.4rem] md:text-[1.4rem] mt-[0.4rem]",
         "input-custom",
-        className
+        className,
       )}
       {...props}
     />
